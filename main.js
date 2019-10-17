ad9000c60e844cc2ceb8779ac19e0c32c04a5f01
extensite <31198462+extensite@users.noreply.github.com>
2019-10-17 10:28:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,57 @@
 
-function changeImage(id, link) {
-document.getElementById(id).src = link
-}
+function getID(id) {
+        return document.getElementById(id)
+    }  
+    
+    function Person(id) {
+        this.id = id
+        this.animations = []
+          
+        this.changeImage = function(link) {
+            getID(this.id).querySelector("img").src = link
+        }
+        this.change = function(face) {
+            this.changeImage(links[face])
+        }    
+        this.addAnimation = function(name, time) {
+            this.animations.push({name: name, time: time, getString: function() {
+                return this.name + " " + this.time + "s infinite step-end"
+            }})
+            this.refreshAnimations()
+        }
+        this.removeAnimation = function(name) {
+            this.animations = this.animations.filter(function( obj ) {
+              return obj.name !== name;
+            });
+            this.refreshAnimations()
+        }
+        this.refreshAnimations = function() {
+            animString = this.animations.map((anim) => { return anim.getString() }).join(",")
+            getID(this.id).querySelector("img").style.animation = animString
+        }
+        this.addEffect = function(id, effect) {
+            getID(id).classList.add(effect)
+        }
+        this.removeEffect = function(id, effect) {
+            getID(id).classList.remove(effect)
+        }
+        this.hide = function(id) {
+            addEffect(id, "hidden")
+        }
+        this.show = function(id) {
+            removeEffect(id, "hidden")
+        }
+        this.walk = function()
+        
+    }
 
-function changeFace(face) {
-changeImage("qt", links[face])
-}
-
-links = {normal: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
-changeFace("normal")
+    function loop() {
+        
+        requestAnimationFrame(loop)
+    }
+        
+    links = {normal: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"}
+    qt = new Person("qt")
+    qt.change("normal")
+    qt.addAnimation("bob", 2)
+    qt.addAnimation("jump", 2)
